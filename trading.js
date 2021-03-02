c9a1197aeeba03321c8000e5638d96a09432a4f7
GuillaumeB75 <guillaume.bezie@yahoo.com>
2021-03-02 22:56:17 +0100

    mise à jour

--- trading.js
+++ trading.js
@@ -1,4 +1,4 @@
-let BTC = 39425
+/*let BTC = 39425
 
 if (BTC >= 100000) {
   console.log('il faut vendre')
@@ -8,5 +8,19 @@ else if (BTC <= 20000) {
 }
 else {
   console.log('ne rien faire et obeserver')
-}
+}*/
 /* revu et modifié mardi soir */
+
+let prixDuBTC = 50000 // Prix du BTC en dollars
+let prixPourVendre = 60000
+let prixPourAcheter = 45000
+
+if (prixDuBTC <= prixPourAcheter) {
+  console.log('Acheter du BTC')
+} else if (prixDuBTC >= prixPourVendre) {
+  console.log('Vendre du BTC')
+} else {
+  console.log('Attendre')
+}
+
+console.log('Programme terminée')
--- xor.js
+++ xor.js
@@ -1,4 +1,20 @@
-let a = 5;
+/*let a = 5;
 let b = 3;
 
-console.log(a ^ b);
+console.log(a ^ b);*/
+
+let a = true
+let b = true
+
+// V1
+if ((a || b) && !(a && b)) {
+  console.log('V1: TRUE')
+} else {
+  console.log('V1: FALSE')
+}
+// V2
+if ((a && !b) || (!a && b)) {
+  console.log('V2: TRUE')
+} else {
+  console.log('V2: FALSE')
+}
